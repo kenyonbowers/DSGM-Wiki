a7fad2bc80e67445f39505d9331884733eadd58d
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-25 18:09:57 -0500

    yes

--- articles/topical/js/DSGM Versions/DSGM Version Handler.js
+++ articles/topical/js/DSGM Versions/DSGM Version Handler.js
@@ -1,12 +1,12 @@
 var Title = document.getElementsByTagName("title")[0].innerText.split(" ");
 
 async function getJSON(){
-    if(Title[3] != "Versions"){
+    if(Title[4] != "Versions"){
         var VersionsHolder = document.getElementById("VersionsHolder");
         var article = '';
         var amount = 9;
 
-        var JSON = await fetch('https://bowersindustry.github.io/DSGM-Wiki/articles/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
+        var JSON = await fetch('https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/Article%20Files/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
             return result.json()
         });
         JSON.versions.forEach((version, index) => {
@@ -21,7 +21,42 @@ async function getJSON(){
         })
     }
     else{
-        
+        var v1Holder = document.getElementById("v1");
+        var v1Articles = "";
+        var v2Holder = document.getElementById("v2");
+        var v2Articles = "";
+        var v3Holder = document.getElementById("v3");
+        var v3Articles = "";
+        var v4Holder = document.getElementById("v4");
+        var v4Articles = "";
+        var v5Holder = document.getElementById("v5");
+        var v5Articles = "";
+
+        var JSON = await fetch('https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/Article%20Files/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
+            return result.json()
+        });
+        JSON.versions.forEach((version, index) => {
+            if(version.v == "1"){
+                v1Articles = v1Articles + `<div class="ArticleBackground"><a href="./${JSON.versions[index].version}.html"><img src="../../IMG/DSGM Icons/${JSON.versions[index].image}.png" class="ArticleImg"/><div class="ArticleTitleDiv"><p class="ArticleTitle">${JSON.versions[index].version}</p></div></a></div>`
+            }
+            else if(version.v == "2"){
+                v2Articles = v2Articles + `<div class="ArticleBackground"><a href="./${JSON.versions[index].version}.html"><img src="../../IMG/DSGM Icons/${JSON.versions[index].image}.png" class="ArticleImg"/><div class="ArticleTitleDiv"><p class="ArticleTitle">${JSON.versions[index].version}</p></div></a></div>`
+            }
+            else if(version.v == "3"){
+                v3Articles = v3Articles + `<div class="ArticleBackground"><a href="./${JSON.versions[index].version}.html"><img src="../../IMG/DSGM Icons/${JSON.versions[index].image}.png" class="ArticleImg"/><div class="ArticleTitleDiv"><p class="ArticleTitle">${JSON.versions[index].version}</p></div></a></div>`
+            }
+            else if(version.v == "4"){
+                v4Articles = v4Articles + `<div class="ArticleBackground"><a href="./${JSON.versions[index].version}.html"><img src="../../IMG/DSGM Icons/${JSON.versions[index].image}.png" class="ArticleImg"/><div class="ArticleTitleDiv"><p class="ArticleTitle">${JSON.versions[index].version}</p></div></a></div>`
+            }
+            if(version.v == "5"){
+                v5Articles = v5Articles + `<div class="ArticleBackground"><a href="./${JSON.versions[index].version}.html"><img src="../../IMG/DSGM Icons/${JSON.versions[index].image}.png" class="ArticleImg"/><div class="ArticleTitleDiv"><p class="ArticleTitle">${JSON.versions[index].version}</p></div></a></div>`
+            }
+        })
+        v1Holder.innerHTML = v1Holder.innerHTML + v1Articles;
+        v2Holder.innerHTML = v2Holder.innerHTML + v2Articles;
+        v3Holder.innerHTML = v3Holder.innerHTML + v3Articles;
+        v4Holder.innerHTML = v4Holder.innerHTML + v4Articles;
+        v5Holder.innerHTML = v5Holder.innerHTML + v5Articles;
     }
 }
 
--- articles/topical/js/DSGM Versions/downloader.js
+++ articles/topical/js/DSGM Versions/downloader.js
@@ -5,39 +5,37 @@ var ext = "";
 var title = document.getElementsByTagName("title")[0].innerText.split(" ");
 
 async function getEXT(){
-	var JSON = await fetch('https://bowersindustry.github.io/DSGM-Wiki/articles/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
+	var JSON = await fetch('https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/Article%20Files/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
 		return result.json();
 	});
-	console.log(JSON)
 	JSON.versions.forEach((version, index) => {
-		console.log(JSON.versions[index].ext)
 		if(version.version == title[4]){
 			ext = version.ext;
-			console.log(version.ext)
 		}
 	});
 
-	console.log(ext)
-
-	filename = "DSGM " + document.getElementsByTagName("title")[0].innerText.split(" ") + "." + ext;
+	filename = "DSGM " + title[4] + "." + ext;
+	FetchTheRest();
 }
 getEXT();
 
 var anchor = document.createElement("a");
 
-fetch(`https://bowersindustry.github.io/DSGM-Wiki/files/DSGM%20Downloads/DSGM%20${filename.split(" ")[1]}`)
-	.then(res => res.blob())
-	.then(blob => {
-		document.body.append(anchor);
-		anchor.style = "display: none;";
+async function FetchTheRest(){
+	fetch(`https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/DSGM%20Downloads/${filename}`)
+		.then(res => res.blob())
+		.then(blob => {
+			document.body.append(anchor);
+			anchor.style = "display: none;";
 
-        var url = window.URL.createObjectURL(blob)
-		anchor.href = url;
-		anchor.download = filename;
+        	var url = window.URL.createObjectURL(blob)
+			anchor.href = url;
+			anchor.download = filename;
 
-		button.style = "background-color: #00ff00;"
-	}
-);
+			button.style = "background-color: #00ff00;"
+		}
+	);
+}
 
 dwnldBtn.addEventListener("click", function(){
     anchor.click();
