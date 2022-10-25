93568dfd3899934a2c7f89db2cd6cc9866dc66fb
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-25 17:44:05 -0500

    yes

--- articles/topical/js/DSGM Versions/downloader.js
+++ articles/topical/js/DSGM Versions/downloader.js
@@ -1,16 +1,28 @@
 var dwnldBtn = document.getElementById("button");
 var button = document.getElementById("downloadButton");
-var JSON = await fetch('https://bowersindustry.github.io/DSGM-Wiki/articles/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
-	return result.json();
-});
+var filename = "";
 var ext = "";
-JSON.versions.forEach((version, index) => {
-	if(version.version == document.getElementsByTagName("title")[0].innerText.split(" ")[4]){
-		ext = version.ext;
-	}
-});
+var title = document.getElementsByTagName("title")[0].innerText.split(" ");
 
-var filename = "DSGM " + document.getElementsByTagName("title")[0].innerText.split(" ")[4] + ext;
+async function getEXT(){
+	var JSON = await fetch('https://bowersindustry.github.io/DSGM-Wiki/articles/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
+		return result.json();
+	});
+	console.log(JSON)
+	JSON.versions.forEach((version, index) => {
+		console.log(JSON.versions[index].ext)
+		if(version.version == title[4]){
+			ext = version.ext;
+			console.log(version.ext)
+		}
+	});
+
+	console.log(ext)
+
+	filename = "DSGM " + document.getElementsByTagName("title")[0].innerText.split(" ") + "." + ext;
+}
+getEXT();
+
 var anchor = document.createElement("a");
 
 fetch(`https://bowersindustry.github.io/DSGM-Wiki/files/DSGM%20Downloads/DSGM%20${filename.split(" ")[1]}`)
