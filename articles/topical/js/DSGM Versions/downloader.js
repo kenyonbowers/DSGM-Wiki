9691c6a962df158015f16e4f6757a9bb008fcbee
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-10-25 21:28:18 -0500

    Update downloader.js

--- articles/topical/js/DSGM Versions/downloader.js
+++ articles/topical/js/DSGM Versions/downloader.js
@@ -3,26 +3,6 @@ var filename = "";
 var ext = "";
 var title = document.getElementsByTagName("title")[0].innerText.split(" ");
 
-/*
-async function getEXT(){
-	var JSON = await fetch('https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/Article%20Files/topical/DSGM%20Versions/DSGM%20Versions%20Available.json').then((result) => {
-		return result.json()
-	});
-	JSON.versions.forEach((version, index) => {
-		if(version.version == title[4]){
-			ext = version.ext;
-		}
-	});
-	console.log(ext)
-	if(ext != ""){
-		filename = "DSGM " + title[4] + "." + ext;
-		FetchTheRest();
-	}
-}
-getEXT();
-
-var anchor = document.createElement("a");
-*/
 async function FetchTheRest(){
 	fetch(`https://raw.githubusercontent.com/BowersIndustry/DSGM-Wiki-Files/main/DSGM%20Downloads/${filename}`)
 		.catch((err) => {
